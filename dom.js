d4b5280a2c956fd655535b023b321cd65e03c677
trickstival <patrick@logcomex.com>
2019-01-29 10:00:18 -0200

    added scrollTo

--- dom.js
+++ dom.js
@@ -6,3 +6,25 @@ export const closest = (el, selector) => {
         el = el.parentElement
     }
 }
+
+/**
+ * Scrolls to element
+ * @param {String|Element} targetEl Element or query selector
+ */
+export const scrollTo = (targetEl) => {
+    const el = typeof targetEl === 'string' 
+        ? document.querySelector(targetEl)
+        : targetEl
+
+    const behavior = 'smooth'
+    if (el.scrollIntoView) {
+        el.scrollIntoView({ behavior, block: 'start' })
+        return
+    }
+    const elementY = window.pageYOffset + el.getBoundingClientRect().top
+    window.scroll({
+        top: elementY,
+        left: 0,
+        behavior
+    })
+}
